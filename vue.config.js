aaf875dc1380abbba90bd8dcf64095878a765f68
serein <619030878@qq.com>
2021-02-25 14:25:21 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -17,5 +17,5 @@ module.exports = {
     },
     productionSourceMap: !(process.env.VUE_APP_ENV === "prod"),
     outputDir: "docs",
-    publicPath: process.env.VUE_APP_ENV === "prod" ? "https://13886912431.github.io/mall-app.github.io/" : "/"
+    publicPath: process.env.VUE_APP_ENV === "prod" ? "https://13886912431.github.io/mall-app/" : "/"
 };
